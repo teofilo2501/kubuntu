2ecfe851d9a742fb23f9560e99a706464e3a7ba2
teofilo2501 <68516123+teofilo2501@users.noreply.github.com>
2021-04-21 17:14:17 -0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -31,6 +31,7 @@ sudo apt-get update
 sudo apt-get install teamviewer -y 
 
 echo "install sublime-text"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo sh -c "echo 'deb https://download.sublimetext.com/ apt/stable/' >> /etc/apt/sources.list.d/sublime-text.list"
 sudo apt-get update
 sudo apt-get install sublime-text -y
