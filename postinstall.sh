4e630d4470e76c150549674bd6f480554a7f36a8
teofilo2501 <68516123+teofilo2501@users.noreply.github.com>
2021-04-21 17:33:06 -0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -37,12 +37,14 @@ sudo apt-get update
 sudo apt-get install sublime-text -y
 
 echo "install vscode"
-sudo apt update
-sudo apt install software-properties-common apt-transport-https wget -y
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add --
-sudo add-apt-repository “deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main”
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code
+
 
 echo "install kodi"
 sudo apt-get install software-properties-common -y
