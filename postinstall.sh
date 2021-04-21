e14ce635461a2e27f742bd8d1a7e520e490d8167
teofilo2501 <68516123+teofilo2501@users.noreply.github.com>
2021-04-21 00:04:34 -0300

    refix anydesk install

--- postinstall.sh
+++ postinstall.sh
@@ -20,7 +20,7 @@ sudo apt-get install -f -y
 
 echo "install anydesk"
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
-sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+sudo sh -c "echo 'deb http://deb.anydesk.com/ all main' > /etc/apt/sources.list.d/anydesk-stable.list"
 sudo apt update
 sudo apt install anydesk -y
 
